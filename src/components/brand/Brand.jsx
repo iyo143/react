ec1204c4e69e0d17703374dea524bf65869c6489
iyo143 <limlimgo14@gmail.com>
2022-02-24 09:11:52 +0800

    no message

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import './brand.css';
+import { google, slack, atlassian, dropbox, shopify } from './imports';
 
 const Brand = () => {
   return (
-    <div>Brand</div>
+    <div className='gpt3__brand section__padding'>
+      <div className='gpt__brand-img-container'>
+        <img src={google} alt="" />
+      </div>
+      <div className='gpt__brand-img-container'>
+        <img src={slack} alt="" />
+      </div >
+      <div className='gpt__brand-img-container'>
+        <img src={atlassian} alt="" />
+      </div>
+      <div className='gpt__brand-img-container'>
+        <img src={dropbox} alt="" />
+      </div>
+      <div className='gpt__brand-img-container'>
+        <img src={shopify} alt="" />
+      </div>
+    </div>
   )
 }
 
